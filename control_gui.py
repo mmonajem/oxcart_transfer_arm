06830f28e0c3d1e0ce3367585a98636f6da994aa
dasistharsh <harsh.pandey@fau.de>
2023-03-10 10:31:58 +0100

    added code documentation for python code

--- control_gui.py
+++ control_gui.py
@@ -6,8 +6,16 @@ from functools import partial
 
 
 class controlGUI():
+    '''
+        This class is responsible to building the GUI interface
+        by utilizing the tkinter library. 
+    '''
 
     def __init__(self):
+        '''
+            Init function (constructor) for setting up the
+            class objects and initilazation parameters.
+        '''
         self.root = Tk()
         self.obj = control()
         self.root.geometry("450x350")
@@ -16,40 +24,125 @@ class controlGUI():
         self.rotationMode = "step"
     
     def setLabel(self):
+        '''
+            This function is responsible for setting the 
+            position of intial state(text display) of GUI.
+            Args:
+                Does not accept any arguments 
+            Returns:
+                Does not return anything
+        '''
         self.label = Label(self.root,text = "Control Ready")
         self.label.place(x=160, y=270)
 
     def setGUILabel(self):
+        '''
+            This function sets the GUI label to HW
+            when HW(Joystick) mode is set.
+            Args:
+                Does not accept any arguments 
+            Returns:
+                Does not return anything
+        '''
         self.toggle.config(text = "HW")
 
     def setGUILabel(self):
+        '''
+            This function sets the GUI label to HW
+            when Continous(motor rotates continiously 
+            if joystick is kept pressed) mode is set.
+            Args:
+                Does not accept any arguments 
+            Returns:
+                Does not return anything
+        '''
         self.toggle.config(text = "continous")
 
     def moveUP(self):
+        '''
+            This function is callback function and is triggered
+            when UP button is pressed. It sends the command
+            to the arduino to rotate the vertical motor up
+            using softwarControl helper library. 
+            Args:
+                Does not accept any arguments 
+            Returns:
+                Does not return anything
+
+        '''
         self.label.config(text = "MOVE UP")
         self.obj.sendCommand("UP")
         
     def moveDown(self):
+        '''
+            This function is callback function and is triggered
+            when DOWN button is pressed. It sends the command
+            to the arduino to rotate the vertical motor down
+            using softwarControl helper library. 
+            Args:
+                Does not accept any arguments 
+            Returns:
+                Does not return anything
+
+        '''
         self.label.config(text = "MOVE DOWN")
         self.obj.sendCommand("DOWN")
 
     def rotateLeft(self):
+        '''
+            This function is callback function and is triggered
+            when LEFT button is pressed. It sends the command
+            to the arduino to rotate the horizontal motor left
+            using softwarControl helper library. 
+            Args:
+                Does not accept any arguments 
+            Returns:
+                Does not return anything
+
+        '''
         self.label.config(text = "ROTATE LEFT")
         self.obj.sendCommand("LEFT")
 
     def rotateRight(self):
+        '''
+            This function is callback function and is triggered
+            when RIGHT button is pressed. It sends the command
+            to the arduino to rotate the horizontal motor right
+            using softwarControl helper library. 
+            Args:
+                Does not accept any arguments 
+            Returns:
+                Does not return anything
+
+        '''
         self.label.config(text = "ROTATE RIGHT")
         self.obj.sendCommand("RIGHT")
 
     def reset(self):
-        self.label.config(text = "RESET")
-        self.obj.sendCommand("RESET")
+        '''
+            This function is callback function and is triggered
+            when reset button is pressed. It sends the reset command
+            to the arduino using softwarControl helper library. 
+            Args:
+                Does not accept any arguments 
+            Returns:
+                Does not return anything
 
-    def reset(self):
+        '''
         self.label.config(text = "RESET")
         self.obj.sendCommand("RESET")
     
     def changeControlMode(self):
+        '''
+            This function is responsible for setting the mode(Joystick/GUI)
+            through the GUI. You can change the state from GUI to Joystick
+            by pressing the mode change button on GUI.Changing the mode back
+            from Joystick to GUI has to be done from the push button.
+            Args:
+                Does not accept any arguments 
+            Returns:
+                Does not return anything
+        '''
         if self.controlMode == "GUI":
             self.label.config(text = "Joystick Mode",fg = "black")
             self.obj.sendCommand("HW")
@@ -60,6 +153,18 @@ class controlGUI():
             self.controlMode = "GUI"
 
     def changeRotationMode(self):
+        '''
+            This function is responsible for setting the mode(step/continous)
+            through the GUI.
+            Step Mode: The motor rotates in steps in the direction based
+                       on joystick input(Default set to 45 degrees)
+            Continious Mode: The motor rotates continously in the direction
+                             based on the input from joystick
+            Args:
+                Does not accept any arguments 
+            Returns:
+                Does not return anything
+        '''
         if self.rotationMode == "step":
             self.label.config(text = "Continious Mode",fg = "black")
             self.obj.sendCommand("continous")
@@ -70,10 +175,26 @@ class controlGUI():
             self.rotationMode = "step"
 
     def defineHeader(self):
+        '''
+            This fucntion sets the header of the GUI.
+            Args:
+                Does not accept any arguments 
+            Returns:
+                Does not return anything
+
+        '''
         label = Label(self.root,text = "ATOM PROBE TOMOGRAPHY",font='Helvetica 18 bold')
         label.pack(pady=20)
 
     def defineSubHeader(self):
+        '''
+            This function sets the sub-header of the GUI.
+            Args:
+                Does not accept any arguments 
+            Returns:
+                Does not return anything
+
+        '''
         label = Label(self.root,text = "Welcome to the Control Tool",font='Helvetica 18 bold')
         label.pack(pady=10)
     
--- softwareControl.py
+++ softwareControl.py
@@ -3,6 +3,11 @@ sys.path.append("/usr/local/lib/python3.7/site-packages/")
 import serial.tools.list_ports
 
 class control:
+    '''
+        This class is responsible for providing helping functions
+        to communicate between the PC and Arduino over serial when
+        the system is set in GUI mode.
+    '''
 
     def __init__(self) -> None:
         self.ports = serial.tools.list_ports.comports()
@@ -11,15 +16,46 @@ class control:
         self.setup()
 
     def printAllPorts(self):
+        '''
+            This function prints all the ports of the 
+            PC for serial communication between arduino
+            and PC.
+            Args:
+                Does not accept any arguments 
+            Returns:
+                Does not return anything
+        '''
         for onePort in self.ports:
             self.portsList.append(str(onePort))
             print(str(onePort))
 
     def setup(self):
+        '''
+            This function setups the serial communication 
+            parameter like baudrate and portId
+
+            TODO: automate port selection instead of static definition
+
+            Args:
+                Does not accept any arguments 
+            Returns:
+                Does not return anything
+        '''
         self.serialInst.baudrate = 9600
         self.serialInst.port = "/dev/cu.usbmodem141201"
         self.serialInst.open()
 
     def sendCommand(self,command):
+        '''
+            This function is responsible for writing/Sending
+            the command from the GUI(PC) to the arduino through
+            serial write.
+            Args:
+                command: command to be written onto serial (type: string)
+            Return:
+                Does not return anaything
+            Format:
+                command: RESTART 
+        '''
         print("COMMAND SENT: ",command)
         self.serialInst.write(command.encode('utf-8'))
